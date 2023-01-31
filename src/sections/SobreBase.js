47c2ed499ca396b37e0496a0493c0aa1e53f49b9
sourbiskit <ruanvictor.qa@gmail.com>
2023-01-31 10:55:06 -0300

    TextBox deleted

--- src/sections/SobreBase.js
+++ src/sections/SobreBase.js
@@ -1,25 +1,19 @@
 import "./SobreBase.css"
-import TextBox from "../components/TextBox";
 import ImgBox from "../components/ImgBox";
 import img1 from "../img/sobre-base1.png";
 import img2 from "../img/sobre-base2.png";
 
 function SobreBase() {
-    const paragrafos = [
-        ["Sempre atenta as necessidades de seus clientes e do mercado a Notek " +
-         "se destaca pelo diferencial no atendimento e pelas estratégias " +
-         "utilizadas, baseada nas experiências de suas gestoras."],
-        ["Transparência, ética e comprometimento é a nossa base para estrutura " +
-         "a base do seu negócio!",
-         "Contabilidade Inteligente é poder contar conosco sempre que precisar."]
-    ]
     const bgColorStyle = "rgba(1, 174, 240, 0.5)";
 
     return (
         <section className="sobre-base">
             <div className="sobre-base--container notek-container desktop">
-                <div className="sobre-base--item">
-                    <TextBox paragrafos={paragrafos[0]} />
+                <div className="sobre-base--item blue-border text-box">
+                    <h3>
+                        Sempre atenta as necessidades de seus clientes e do mercado a Notek se destaca pelo
+                        diferencial no atendimento e pelas estratégias utilizadas, baseada nas experiências de suas gestoras.
+                    </h3>
                 </div>
                 <div className="sobre-base--item">
                     <ImgBox bgColorStyle={bgColorStyle} bgImage={img1} />
@@ -27,8 +21,13 @@ function SobreBase() {
                 <div className="sobre-base--item">
                     <ImgBox bgColorStyle={bgColorStyle} bgImage={img2} />
                 </div>
-                <div className="sobre-base--item">
-                    <TextBox paragrafos={paragrafos[1]} />
+                <div className="sobre-base--item blue-border text-box">
+                    <h3>
+                        Transparência, ética e comprometimento é a nossa base para estrutura a base do seu negócio!
+                    </h3>
+                    <h3>
+                        Contabilidade Inteligente é poder contar conosco sempre que precisar.
+                    </h3>
                 </div>
             </div>
             
@@ -36,14 +35,22 @@ function SobreBase() {
                 <div className="sobre-base--item item-img">
                     <ImgBox bgColorStyle={bgColorStyle} bgImage={img1} />  
                 </div>
-                <div className="sobre-base--item item-text">
-                    <TextBox paragrafos={paragrafos[0]} />
+                <div className="sobre-base--item item-text blue-border text-box">
+                    <h3>
+                        Sempre atenta as necessidades de seus clientes e do mercado a Notek se destaca pelo
+                        diferencial no atendimento e pelas estratégias utilizadas, baseada nas experiências de suas gestoras.
+                    </h3>
                 </div>
                 <div className="sobre-base--item item-img">
                     <ImgBox bgColorStyle={bgColorStyle} bgImage={img2} />
                 </div>
-                <div className="sobre-base--item item-text">
-                    <TextBox paragrafos={paragrafos[1]} />
+                <div className="sobre-base--item item-text blue-border text-box">
+                    <h3>
+                        Transparência, ética e comprometimento é a nossa base para estrutura a base do seu negócio!
+                    </h3>
+                    <h3>
+                        Contabilidade Inteligente é poder contar conosco sempre que precisar.
+                    </h3>
                 </div>
             </div>
         </section>
