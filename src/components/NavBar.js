77f645a9ea306fbc61812533d8c42f6fc3741795
sourbiskit <ruanvictor.qa@gmail.com>
2023-02-09 12:37:56 -0300

    NavLinks mudança

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,26 +6,52 @@ import Navbar from 'react-bootstrap/Navbar';
 
 
 function NavBar() {
+    const linksContent = [
+        {
+            title: "Serviços",
+            link: "/servicos"
+        },
+        {
+            title: "Sobre",
+            link: "/sobre"
+        },
+        {
+            title: "Tabelas",
+            link: "/tabelas"
+        },
+        {
+            title: "Notícias",
+            link: "/noticias"
+        },
+        {
+            title: "Contato",
+            link: "/contato"
+        },
+    ];
+    const generateNavLinks = linksContent.map((value, index) =>
+        <Nav.Link
+            key={index}
+            href={value.link} 
+            className="active" 
+        >
+            {value.title}
+        </Nav.Link>
+    )
+
     return (
-        <>
-            <Navbar className="nav" expand="lg" sticky="top" variant="light">
-                <Container>
-                    <Navbar.Brand href="/"><img id="nav--logo" src={navLogo} alt="Notek Logo" /></Navbar.Brand>
-                    
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="nav--Nav.Links">
-                            <Nav.Link href="/servicos" className="active" >Serviços</Nav.Link>
-                            <Nav.Link href="/sobre" className="active" >Sobre</Nav.Link>
-                            <Nav.Link href="/tabelas" className="active" >Tabelas</Nav.Link>
-                            <Nav.Link href="#home" className="active" >Links Úteis</Nav.Link>
-                            <Nav.Link href="/noticias" className="active" >Notícias</Nav.Link>
-                            <Nav.Link href="/contato" className="active" >Contato</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
+        <Navbar className="nav" expand="lg" sticky="top" variant="light">
+            <Container>
+                <Navbar.Brand href="/"><img id="nav--logo" src={navLogo} alt="Notek Logo" /></Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="nav--Nav.Links">
+                        {generateNavLinks}
+                        <Nav.Link href="#home" className="active" >Links Úteis</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
