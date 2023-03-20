2fcf43ed6a55cfded2a45770a69c36c4ffc910dd
M1SS1NGHU3S <ruanvictor.qa@gmail.com>
2023-03-19 21:06:43 -0300

    BlogContent desktop

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,10 +1,23 @@
 import "./BlogCard.css";
 
 function BlogCard(props) {
+    const isBlogContentStyle = {
+        height: "auto"
+    };
+    const isBlogImgStyle = {
+        width: "inherit",
+        height: "200px",
+        objectFit: "cover",
+        borderRadius: "20px 20px 0 0",
+    };
+    const isBlogCardStyle = {
+        marginBlock: "30px"
+    };
+
     return (
-        <div className="blog-card noticias-row--column">
-            <img className="blog-card--img" src={props.img} alt={props.alt} />
-            <div className="blog-card--content card-content">
+        <div className="blog-card noticias-row--column" style={props.isBlog && isBlogCardStyle}>
+            <img className="blog-card--img"  style={props.isBlog && isBlogImgStyle} src={props.img} alt={props.alt} />
+            <div className="blog-card--content card-content" style={props.isBlog && isBlogContentStyle}>
                 <a href="/" className="blog-card--link">
                     <h3 className="blog-card--title">{props.titulo}</h3>
                 </a>
--- src/sections/BlogPage.js
+++ src/sections/BlogPage.js
@@ -4,6 +4,7 @@ import {
 } from "../sections-data/Imports";
 import {
     NoticiaLink,
+    BlogCard
 } from "../components/Imports";
 import {
     blogExample
@@ -13,6 +14,7 @@ function BlogPage(props) {
     return (
         <div className="container blog-container">
             <BlogHeader />
+            <BlogContent />
         </div>
     );
 }
@@ -30,12 +32,76 @@ function BlogHeader() {
     return (
         <section className="blog-header">
             <img className="blog-header--img" src={blogExample} alt="person writing in a gray ambient" />
-            
+
             <div className="blog-header--noticias">
-                <h3 className="blog-header--noticias-title">
+                <h3 className="blog--recentes-title">
                     Notícias recentes
                 </h3>
                 {generateLinks}
+            </div>
+        </section>
+    );
+}
+
+function BlogContent() {
+    const generateCards = noticiasRow.cardsContent.map((value, index) =>
+        <BlogCard 
+            key={index}
+            img={value.img}
+            titulo={value.titulo}
+            altText={value.altText}
+            isBlog={true}
+        />
+    )
+
+    return (
+        <section className="blog-content">
+            <div className="blog-content--left">
+                <h2 className="blog-content--title">Título do Blog</h2>
+                <hr className="blog-content--hr" />
+
+                <p className="blog-content--paragraph">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing 
+                    elit. Maecenas ac metus a turpis iaculis mattis. 
+                    Class aptent taciti sociosqu ad litora torquent 
+                    per conubia nostra, per inceptos himenaeos. Vivamus 
+                    sem magna, porttitor in mattis id, ultricies non 
+                    lacus. Suspendisse vitae eleifend neque, a commodo 
+                    dolor. Fusce dapibus justo ut neque accumsan 
+                    lobortis. Maecenas tempor sapien nisl, ut iaculis leo 
+                    posuere ac. Nam dictum magna et ligula dapibus, vel 
+                    pharetra nisi interdum. Proin efficitur, nulla eget 
+                    feugiat interdum, velit mi porttitor augue, in 
+                    laoreet lorem justo id ipsum.
+                </p>
+                <p className="blog-content--paragraph">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing 
+                    elit. Suspendisse feugiat fermentum neque sed 
+                    volutpat. Praesent scelerisque enim ac mauris 
+                    ultricies laoreet. Morbi dapibus metus erat, quis 
+                    varius risus consequat ut. Praesent non risus 
+                    convallis tellus elementum fringilla et vel mi. Ut 
+                    laoreet diam pulvinar dapibus rhoncus. Duis feugiat 
+                    sollicitudin sapien, sed sodales mauris rhoncus in. 
+                    Suspendisse eget ultricies felis. Curabitur eu tellus 
+                    eget risus eleifend euismod. Vivamus nibh massa, 
+                    tristique nec ligula eu, accumsan gravida urna. 
+                    Donec sit amet ligula nec urna pretium malesuada.
+                </p>
+                <p className="blog-content--paragraph">
+                    Aliquam tellus purus, cursus nec dapibus eget, 
+                    vehicula eget nibh. Proin faucibus sit amet ante non 
+                    condimentum. Aliquam a molestie lorem, at pharetra 
+                    arcu. Lorem ipsum dolor sit amet, consectetur 
+                    adipiscing elit. Curabitur nec mauris diam. Donec et 
+                    justo massa. Proin est eros, finibus sit amet mi vel, 
+                    aliquam sagittis sem. Nam malesuada nisi nec dui 
+                    cursus sollicitudin. 
+                </p>
+            </div>
+            <div className="blog-content--right">
+                <h3 className="blog--recentes-title">Artigos Recentes</h3>
+                {generateCards}
             </div>
         </section>
     );
