24aca99c1e5ec5c68a7cedb86278332754662dbb
M1SS1NGHU3S <ruanvictor.qa@gmail.com>
2025-01-29 18:25:35 -0300

    Whatsapp floating icon

--- src/App.js
+++ src/App.js
@@ -4,10 +4,39 @@ import {
 	NavBar,
 } from "./components/Imports"
 import {Outlet, useLocation} from "react-router-dom";
+import { useEffect } from "react";
 // import { Manutencao } from "./pages/Imports";
 
 function App() {
 	const location = useLocation();
+
+	window.rwbp = {
+		email:'cris@notekcontabil.com.br',
+		phone:'5511950618623',
+		message:'Olá, seja bem vindo! Como posso te ajudar ?',
+		lang:'pt-BR'
+	}
+
+	useEffect(() => {
+		const script1 = document.createElement('script');
+
+		script1.defer = true;
+		script1.async = true;
+		script1.src = "https://duz4dqsaqembt.cloudfront.net/client/whats.js";
+
+		window.rwbp = {
+			email:'cris@notekcontabil.com.br',
+			phone:'5511950618623',
+			message:'Olá, seja bem vindo! Como posso te ajudar ?',
+			lang:'pt-BR'
+		};
+	  
+		document.body.appendChild(script1);
+	  
+		return () => {
+		  document.body.removeChild(script1);
+		}
+	  }, []);
 
 	return (
 		<div>
